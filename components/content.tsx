00424bb807950648d2f669a7ec1d07f510d152d5
Serhii Didenko <srg.dd0@gmail.com>
2025-04-01 17:03:47 +0300

    feat: redesign home page

---
+++ components/content.tsx
@@ -0,0 +1,30 @@
+import { Button } from '@/components/ui/button'
+import { ChevronRight } from 'lucide-react'
+import Link from 'next/link'
+
+export default function ContentSection() {
+  return (
+    <section className='py-16 md:py-32'>
+      <div className='mx-auto max-w-5xl px-6'>
+        <div className='grid gap-6 md:grid-cols-2 md:gap-12'>
+          <h2 className='text-4xl font-medium'>What is Centria Hub?</h2>
+          <div className='space-y-6'>
+            <p>
+              Centria Hub is a centralized digital platform that integrates all
+              university resources—news, events, articles, and emails—into one
+              intuitive space. It enhances accessibility, collaboration, and
+              engagement for students, staff, and faculty.
+            </p>
+          </div>
+          <h2 className='text-4xl font-medium'>Mission</h2>
+          <div className='space-y-6'>
+            <p>
+              To simplify access to university resources, improve communication,
+              and create a more connected campus experience.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
---
+++ components/team.tsx
@@ -0,0 +1,53 @@
+import Image from 'next/image'
+
+const members = [
+  {
+    name: 'Serhii Didenko',
+    role: 'PO & Developer',
+    avatar: '/serhii.png',
+  },
+  {
+    name: 'Misato',
+    role: 'Developer',
+    avatar: '/misato.png',
+  },
+  {
+    name: 'Niharika',
+    role: 'PM & Tester',
+    avatar: '',
+  },
+]
+
+export default function TeamSection() {
+  return (
+    <section className='py-12 md:py-24'>
+      <div className='mx-auto max-w-3xl px-8 lg:px-0'>
+        <h2 className='mb-8 text-4xl font-bold md:mb-16 lg:text-5xl'>
+          Our team
+        </h2>
+        <div>
+          <div className='flex justify-evenly items-center text-center gap-4 border-t py-6 md:grid-cols-4'>
+            {members.map((member, index) => (
+              <div key={index}>
+                <div className='bg-background size-20 rounded-full border p-0.5 shadow shadow-zinc-950/5'>
+                  <Image
+                    className='aspect-square rounded-full object-cover w-full'
+                    src={member.avatar}
+                    alt={member.name}
+                    height='460'
+                    width='460'
+                    loading='lazy'
+                  />
+                </div>
+                <span className='mt-2 block text-sm'>{member.name}</span>
+                <span className='text-muted-foreground block text-xs'>
+                  {member.role}
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
