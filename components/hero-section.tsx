04c366b5204a73412bc06515b5b658d6fe404d81
Serhii Didenko <srg.dd0@gmail.com>
2025-04-10 13:44:01 +0300

    fix: fix hero component types

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -6,22 +6,18 @@ import Image from 'next/image'
 import { TextEffect } from '@/components/ui/text-effect'
 import { AnimatedGroup } from '@/components/ui/animated-group'
 
-const transitionVariants = {
-  item: {
-    hidden: {
-      opacity: 0,
-      filter: 'blur(12px)',
-      y: 12,
-    },
-    visible: {
-      opacity: 1,
-      filter: 'blur(0px)',
-      y: 0,
-      transition: {
-        type: 'spring',
-        bounce: 0.3,
-        duration: 1.5,
-      },
+const itemVariant = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: 'spring',
+      bounce: 0.3,
+      duration: 2,
     },
   },
 }
@@ -41,26 +37,10 @@ export const HeroSection = () => {
           <div className='relative pt-24 md:pt-36'>
             <AnimatedGroup
               variants={{
-                container: {
-                  visible: {
-                    transition: {
-                      delayChildren: 1,
-                    },
-                  },
-                },
-                item: {
-                  hidden: {
-                    opacity: 0,
-                    y: 20,
-                  },
-                  visible: {
-                    opacity: 1,
-                    y: 0,
-                    transition: {
-                      type: 'spring',
-                      bounce: 0.3,
-                      duration: 2,
-                    },
+                hidden: {},
+                visible: {
+                  transition: {
+                    delayChildren: 1,
                   },
                 },
               }}
@@ -77,13 +57,20 @@ export const HeroSection = () => {
             <div className='absolute inset-0 -z-10 size-full [background:radial-gradient(125%_125%_at_50%_100%,transparent_0%,var(--color-background)_75%)]'></div>
             <div className='mx-auto max-w-7xl px-6'>
               <div className='text-center sm:mx-auto lg:mr-auto lg:mt-0'>
-                <AnimatedGroup variants={transitionVariants}>
+                <AnimatedGroup
+                  variants={{
+                    hidden: {},
+                    visible: {
+                      transition: {
+                        delayChildren: 1,
+                      },
+                    },
+                  }}>
                   <span className='hover:bg-background dark:hover:border-t-border bg-muted group mx-auto flex w-fit items-center gap-4 rounded-full border p-1 pl-4 shadow-md shadow-zinc-950/5 transition-colors duration-300 dark:border-t-white/5 dark:shadow-zinc-950'>
                     <span className='text-foreground text-sm'>
                       University in your phone
                     </span>
                     <span className='dark:border-background block h-4 w-0.5 border-l bg-white dark:bg-zinc-700'></span>
-
                     <div className='bg-background group-hover:bg-muted size-6 overflow-hidden rounded-full duration-500'>
                       <div className='flex w-12 -translate-x-1/2 duration-500 ease-in-out group-hover:translate-x-0'>
                         <span className='flex size-6'>
@@ -118,16 +105,15 @@ export const HeroSection = () => {
 
                 <AnimatedGroup
                   variants={{
-                    container: {
-                      visible: {
-                        transition: {
-                          staggerChildren: 0.05,
-                          delayChildren: 0.75,
-                        },
+                    hidden: {},
+                    visible: {
+                      transition: {
+                        staggerChildren: 0.05,
+                        delayChildren: 0.75,
                       },
                     },
-                    ...transitionVariants,
                   }}
+                  itemVariants={itemVariant}
                   className='mt-12 flex flex-col items-center justify-center gap-2 md:flex-row'>
                   <div
                     key={1}
@@ -157,16 +143,15 @@ export const HeroSection = () => {
 
             <AnimatedGroup
               variants={{
-                container: {
-                  visible: {
-                    transition: {
-                      staggerChildren: 0.05,
-                      delayChildren: 0.75,
-                    },
+                hidden: {},
+                visible: {
+                  transition: {
+                    staggerChildren: 0.05,
+                    delayChildren: 0.75,
                   },
                 },
-                ...transitionVariants,
-              }}>
+              }}
+              itemVariants={itemVariant}>
               <div className='relative -mr-56 mt-8 overflow-hidden px-2 sm:mr-0 sm:mt-12 md:mt-20'>
                 <div
                   aria-hidden
